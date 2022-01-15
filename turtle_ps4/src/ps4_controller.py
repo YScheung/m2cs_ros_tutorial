f84a7c4024d50624fcdf3652857c62896189d7d2
Jonathan <jonathan.ys.cheung@gmail.com>
2022-01-15 20:47:18 +0800

    Submission 1

--- turtle_ps4/src/ps4_controller.py
+++ turtle_ps4/src/ps4_controller.py
@@ -3,29 +3,86 @@ import rospy
 from geometry_msgs.msg import Twist
 from turtlesim.srv import SetPen
 # hint: some imports are missing
+from std_srvs.srv import Empty, EmptyRequest
+from m2_ps4.msg import Ps4Data 
+
 
 old_data = Ps4Data()
-
+count = 1
 def callback(data):
     global old_data
+    global count
+
+    #ps4 data is passed here
+
+    rospy.loginfo(data.cross) 
     
-    # you should publish the velocity here!
+    if (data.hat_ly != 0):
+        t = Twist()
+        speed = data.hat_ly
+        t.linear.x = speed * count * 0.44 # let constant k be 2
+        #t.linear.x = speed
+        pub.publish(t)
+
+    if (data.l3 == True):
+        count = count + 1   
+
+    if (data.ps == True):
+        clear_path_client()
+
+    if (data.triangle == True):
+        change_color_client(0,220,0,0,0)
+
+    if (data.circle == True):
+        change_color_client(255,0,0,0,0)
+
+    if (data.cross == True):
+        change_color_client(0,0,255,0,0)
+
+    if (data.square == True):
+        change_color_client(128,0,128,0,0) 
+
+
+
+
+    if (data.hat_rx == 1):
+        t = Twist()
+        t.angular.z = 1
+        pub.publish(t)
+
+    if (data.hat_rx == -1):
+        t = Twist()
+        t.angular.z = -1
+        pub.publish(t) 
     
+
+    #if data.square == pressed ...... 
+
+ 
+
+    # you should publish the velocity here
+
     # hint: to detect a button being pressed, you can use the following pseudocode:
-    # 
+    #
     # if ((data.button is pressed) and (old_data.button not pressed)),
     # then do something...
-    
+    #if (data.cross == True) and (old_square.button not pressed)):
+
     old_data = data
 
 if __name__ == '__main__':
     rospy.init_node('ps4_controller')
+
+    pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size = 1)  # publisher object goes here... hint: the topic type is Twist
     
-    pub = # publisher object goes here... hint: the topic type is Twist
-    sub = # subscriber object goes here
-    
+
+    clear_path_client = rospy.ServiceProxy('clear', Empty)
+    change_color_client = rospy.ServiceProxy('turtle1/set_pen', SetPen)
+
+    sub = rospy.Subscriber('input/ps4_data', Ps4Data, callback) # subscriber object goes here
+
     # one service object is needed for each service called!
-    srv_col = # service client object goes here... hint: the srv type is SetPen
+    #srv_col = # service client object goes here... hint: the srv type is SetPen
     # fill in the other service client object...
-    
+
     rospy.spin()
